d089d49887a154e5b80c4ab06805de27d5c6adeb
bigeppo <carlog91cast@gmail.com>
2022-09-19 18:59:39 +0200

    setting seeder but database not found

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Admin\Posts as AdminPosts;
+use App\Models\posts;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 
@@ -12,6 +14,14 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 50; $i++) { 
+            $newPost = new Posts();
+            $newPost->title = $faker->realText(35);
+            $newPost->author = $faker->userName();
+            $newPost->post_date = $faker->dateTimeThisYear();
+            $newPost->post_image = $faker->imageUrl();
+            $newPost->post_content = $faker->paragraph(5, false);
+            $newPost->save();
+        }
     }
-}
+ }
